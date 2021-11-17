569657929fa8c11c64e966fba50519c21d84334a
keka3avr <kekazavr228@gmail.com>
2021-11-18 01:31:06 +0300

    some fixes added

---
+++ src/app/components/Listing/Error.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import React from 'react';
+
+export default function Error() {
+    return <div>something went wrong</div>;
+}
--- src/app/components/Listing/Listing.js
+++ src/app/components/Listing/Listing.js
@@ -4,11 +4,14 @@ import { nanoid } from 'nanoid';
 import React from 'react';
 import ShopCard from './ShopCard/ShopCard';
 import data from '../../data.json';
+import Error from './Error';
 
 export default function Listing() {
     const cardList = data.map((card) => {
         return <ShopCard card={card} key={nanoid()} />;
     });
+
+    if (!cardList[0]) return <Error />;
 
     return <ul className='Listing cards-view_module'>{cardList}</ul>;
 }
