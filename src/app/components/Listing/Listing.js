3654a31e746701661f80feb5ef331246bd007a37
keka3avr <kekazavr228@gmail.com>
2021-12-05 15:27:11 +1100

    null check added

--- src/app/components/Listing/Listing.js
+++ src/app/components/Listing/Listing.js
@@ -4,14 +4,14 @@ import { nanoid } from 'nanoid';
 import React from 'react';
 import ShopCard from './ShopCard/ShopCard';
 import data from '../../data.json';
-import Error from './Error';
+// import Error from './Error';
 
 export default function Listing() {
     const cardList = data.map((card) => {
         return <ShopCard card={card} key={nanoid()} />;
     });
 
-    if (!cardList[0]) return <Error />;
+    if (!cardList[0]) return null;
 
     return <ul className='Listing cards-view_module'>{cardList}</ul>;
 }
